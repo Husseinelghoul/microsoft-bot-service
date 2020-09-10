5dcfd819a7f432c976e3a70cb1abc237466ad781
Hussein El Ghoul <Husseinelghoul@yahoo.com>
2020-09-10 09:06:41 +0300

    last try

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "39f7dc55-7782-4bc5-8543-6db615485789")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "nv5gH~11YgE_KLsZpqR55IApQzB6I-t~__")
+    APP_ID = os.environ.get("MicrosoftAppId", "253b1072-b632-4712-a3c2-2755292b8916")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "~QzgGq74-4z9VW.~q~D5y4un-P6hqEL~Lo")
     QNA_KNOWLEDGEBASE_ID = os.environ.get("QnAKnowledgebaseId", "24bed8e8-7bd7-47e1-9461-b80cc5011a51")
     QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "e074dd5e-82a1-4065-9724-21f9b0cb758c")
     QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "https://globemedbot-poc.azurewebsites.net/qnamaker")
